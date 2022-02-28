af46d6582f1a814f41b56aab0a066b6a9de971c4
eric rosas <erosas.devjava@gmail.com;>
2022-02-28 18:36:56 -0300

    init app

---
+++ src/main/java/com/wzone/MsUsuarioClApplication.java
@@ -0,0 +1,13 @@
+package com.wzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUsuarioClApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUsuarioClApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wzone/MsUsuarioClApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuarioClApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
